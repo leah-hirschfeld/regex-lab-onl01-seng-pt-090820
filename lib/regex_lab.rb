77a13dd64df2f83211957265f464e98fa7e8532b
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-18 02:53:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if (word).scan(/\A+[aeiou]/) == true
+    true 
+  else (word).scan(/\A+[^aeiou]/) == true
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  
 end
 
 def words_five_letters_long(text)
